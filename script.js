38810e90f6603bf276bb0efe320f12bb0b55ebc4
Saad Tebba <116269919+SaadTebba@users.noreply.github.com>
2022-11-24 01:52:36 +0100

    ---

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
 function ajouter() {
 
-    let table = document.getElementById('table');
+  let nomMarqueValidation = /^[aA-zZ ?aA-zZ]{3,30}$/;
+  let prixValidation = /^[0-9]+\$?$/;
+
+  let nom = document.getElementById("nom").value;
+  let marque = document.getElementById("marque").value;
+  let prix = document.getElementById("prix").value;
+  let date = document.getElementById("date").value;
+  let type = document.getElementById("type").value;
+
+  let arr = [];
+
+  if (nomMarqueValidation.test(nom)) {
+    document.getElementById("nomerror").style.visibility = "hidden";
+  } else {
+    document.getElementById("nomerror").style.visibility = "visible";
+    arr.push(1);
+  }
+
+  if (nomMarqueValidation.test(marque)) {
+    document.getElementById("marqueerror").style.visibility = "hidden";
+  } else {
+    document.getElementById("marqueerror").style.visibility = "visible";
+    arr.push(1);
+  }
+
+  if (prixValidation.test(prix)) {
+    document.getElementById("prixerror").style.visibility = "hidden";
+  } else {
+    document.getElementById("prixerror").style.visibility = "visible";
+    arr.push(1);
+  }
+
+  if (arr == 0) {
+
+    let table = document.getElementById("table");
     let row = table.insertRow();
 
     let nom = row.insertCell();
@@ -9,13 +43,22 @@ function ajouter() {
     let date = row.insertCell();
     let type = row.insertCell();
     let non = row.insertCell();
+    let modifier = row.insertCell();
+    let supprimer = row.insertCell();
 
-    nom.innerHTML = document.getElementById('nom').value;
-    marque.innerHTML = document.getElementById('marque').value;
-    prix.innerHTML = document.getElementById('prix').value;
-    date.innerHTML = document.getElementById('date').value;
-    type.innerHTML = document.getElementById('type').value;
-    non.innerHTML = document.getElementById('oui').value;
-    non.innerHTML = document.getElementById('non').value;
 
+    nom.innerHTML = document.getElementById("nom").value;
+    marque.innerHTML = document.getElementById("marque").value;
+    prix.innerHTML = document.getElementById("prix").value;
+    date.innerHTML = document.getElementById("date").value;
+    type.innerHTML = document.getElementById("type").value;
+    non.innerHTML = document.getElementById("oui").value;
+    modifier.innerHTML = document.getElementById("oui");
+    supprimer.innerHTML = document.getElementById("oui");
+
+  }
 }
+
+
+
+// window.scrollBy(100, 0);
